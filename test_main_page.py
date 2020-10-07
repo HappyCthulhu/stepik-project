69027d1f498e0d5c6627aac9130f21f76c2fc4db
HappyCthulhu <valeriyg98@gmail.com>
2020-10-07 13:21:28 +0300

    add assert is_element_present

--- test_main_page.py
+++ test_main_page.py
@@ -1,5 +1,15 @@
+from pages.main_page import MainPage
+
+
 def test_guest_can_go_to_login_page(driver):
     link = "http://selenium1py.pythonanywhere.com/"
-    driver.get(link)
-    login_link = driver.find_element_by_css_selector("#login_link")
-    login_link.click()
+    page = MainPage(driver, link)  # инициализируем Page Object, передаем в конструктор экземпляр драйвера и url адрес
+    page.open()  # открываем страницу
+    page.go_to_login_page()  # выполняем метод страницы - переходим на страницу логина
+
+
+def test_guest_should_see_login_link(driver):
+    link = "http://selenium1py.pythonanywhere.com/"
+    page = MainPage(driver, link)
+    page.open()
+    page.should_be_login_link()
